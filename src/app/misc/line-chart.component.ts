10ff149b3b8ae0c05e3f9eae4129010d7d0610ee
aamirmukaram <aamirmukaram@gmail.com>
2018-11-16 16:46:52 +0500

    added line chart component

--- src/app/misc/line-chart.component.ts
+++ src/app/misc/line-chart.component.ts
@@ -1,15 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  ElementRef,
+  HostListener,
+  Input,
+  OnInit
+} from '@angular/core';
+
+// interface DataPoint {
+//
+// }
 
 @Component({
   selector: 'app-line-chart',
   templateUrl: './line-chart.component.html',
-  styleUrls: ['./line-chart.component.scss']
+  styleUrls: ['./line-chart.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class LineChartComponent implements OnInit {
+export class LineChartComponent implements OnInit, AfterViewInit {
+  @Input('data') public data: Array<any> = [];
+  private el: HTMLElement;
 
-  constructor() { }
+  constructor(elRef: ElementRef, private cdr: ChangeDetectorRef) {
+    this.el = elRef.nativeElement;
+  }
+
+  @HostListener('window:resize')
+  public onResize() {
+    this.drawGraph();
+  }
 
   ngOnInit() {
+  }
+
+  public ngAfterViewInit() {
+    this.drawGraph();
+  }
+
+  private drawGraph() {
+    const height = this.el.clientHeight;
+    const width = this.el.clientWidth;
+
+    console.log('height', 'width', height , width);
   }
 
 }
