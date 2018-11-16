bc2d98a4521d69c5e2b8f3f5ffd22af44a2eed5a
aamirmukaram <aamirmukaram@gmail.com>
2018-11-16 16:20:47 +0500

    added line chart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LineChartModule } from './misc/line-chart.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    LineChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/misc/line-chart.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LineChartComponent } from './line-chart.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    LineChartComponent
+  ],
+  exports: [
+    LineChartComponent
+  ]
+})
+export class LineChartModule {}
